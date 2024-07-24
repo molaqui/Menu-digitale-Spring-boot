76ac7c45ced15cf73a4c723c668a9259141a121b
EL MELLAQUI Abderrahim <abdrahimmolaqui@gmail.com>
2024-07-24 12:13:47 +0100

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
