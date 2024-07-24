4e51e10b1cc7d71d4e466a8ef9ecf6ae872b0df5
EL MELLAQUI Abderrahim <abdrahimmolaqui@gmail.com>
2024-07-24 11:38:03 +0100

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(DemoApplication.class, args);
     }
 }
